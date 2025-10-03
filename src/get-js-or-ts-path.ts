735121d97ffeafa33ec3b0d0e72621d0e6717bfd
MohamedAli00949 <76850143+MohamedAli00949@users.noreply.github.com>
2025-10-03 03:13:39 +0300

    fix: refactor get key path at js or ts

--- src/get-js-or-ts-path.ts
+++ src/get-js-or-ts-path.ts
@@ -1,11 +1,16 @@
-import { findTupleIndex, getIndentationLevel, isJSComment } from "./utils";
+import {
+	cleanLine,
+	findTupleIndex,
+	getIndentationLevel,
+	isJSComment,
+} from "./utils";
 import * as vscode from "vscode";
 
 type BlockType = "object" | "class" | "type" | null;
 
 function getBlockData(
 	trimmedLine: string,
-	equalSperatorLine: number,
+	equalSeparatorLine: number,
 ): {
 	typeName: string;
 	blockType: BlockType;
@@ -28,7 +33,7 @@ function getBlockData(
 		/(?:export\s+)?(?:const|let|var)\s+(\w+)(?:\s*:)?/,
 	);
 	if (constMatch) {
-		if (/:\s*[{[]/.test(trimmedLine) && equalSperatorLine === -1) {
+		if (/:\s*[{[]/.test(trimmedLine) && equalSeparatorLine === -1) {
 			return { typeName: "", blockType: "type" };
 		} else {
 			return { typeName: constMatch[1], blockType: "object" };
@@ -44,7 +49,78 @@ function getBlockData(
 	return null;
 }
 
-// todo: fix if any prop key at the path start with number to add it at ["{key-name}"]
+function buildFinalPath(
+	pathChunks: { content: string; line: number; idx: number }[],
+	blockType: BlockType,
+	typeName: string,
+) {
+	if (blockType === "object" || blockType === "class") {
+		let result = "";
+		pathChunks
+			.sort((a, b) => {
+				if (a.line > b.line) {
+					if (a.idx < b.idx) {
+						return -1;
+					} else {
+						return 1;
+					}
+				} else {
+					if (a.idx < b.idx) {
+						return -1;
+					} else {
+						return 1;
+					}
+				}
+			})
+			.forEach((chunk, i) => {
+				if (isNaN(Number(chunk.content))) {
+					result += `${i !== 0 ? "." : ""}${chunk.content}`;
+				} else {
+					result += `${result.endsWith("]") ? "" : "."}[${chunk.content}]`;
+				}
+			});
+		return typeName + "." + result;
+	} else {
+		return (
+			typeName +
+			"" +
+			pathChunks
+				.sort((a, b) => {
+					if (a.line > b.line) {
+						if (a.idx < b.idx) {
+							return -1;
+						} else {
+							return 1;
+						}
+					} else {
+						if (a.idx < b.idx) {
+							return -1;
+						} else {
+							return 1;
+						}
+					}
+				})
+				.map((p) =>
+					!isNaN(Number(p.content))
+						? `[${p.content}]`
+						: p.content.endsWith("]")
+						? p.content
+								.split("[")
+								.map((p2) => (p2.includes("]") ? `[${p2}` : `["${p2}"]`))
+								.join("")
+						: `["${p.content}"]`,
+				)
+				.join("")
+		);
+	}
+}
+
+interface PathSegment {
+	content: string;
+	line: number;
+	idx: number;
+}
+
 export function getKeyPathAtJSOrTS(
 	documentText: string,
 	selectedText: string,
@@ -52,80 +128,124 @@ export function getKeyPathAtJSOrTS(
 	startLine: number,
 ): { path: string; error: string } {
 	const lines = documentText.split("\n");
+
 	let lastLineLevel = getIndentationLevel(lines[startLine]);
-	let equalSperatorLine = -1;
+	let equalSeparatorLine = -1;
 
 	let blockType: BlockType = null;
 	let typeName = "";
-	const path: string[] = [selectedText];
+
+	const path: PathSegment[] = [
+		{
+			content: selectedText,
+			line: selection.start.line,
+			idx: selection.start.character,
+		},
+	];
 
 	for (let i = startLine; i >= 0; i--) {
 		const line = lines[i];
+		const comemntLine = isJSComment(line);
 
-		if (!line.trim() || isJSComment(line)) {
+		if (!line.trim() || comemntLine.check) {
+			if (i === startLine || comemntLine.type === "block") {
+				return { path: "", error: "Sorry, this is comment" };
+			}
+
 			continue;
 		}
 
-		const rawLine =
-			startLine === i ? line.slice(0, selection.start.character) : line;
+		// Determine which part of the line to analyze
+		const isCurrentLine = i === startLine;
+		const rawLine = isCurrentLine
+			? line.slice(0, selection.start.character)
+			: line;
 
-		const targetLineSlice = (() => {
-			const cleaned = rawLine
-				.replace(/\r/g, "")
-				.replace(/\/\*.*?\*\//g, "")
-				.replace(/\/\/.*$/g, "");
+		const targetLineSlice = cleanLine(rawLine);
 
-			const segments = cleaned
-				.split(";")
-				.map((s) => s.trim())
-				.filter(Boolean);
-
-			return segments.length > 0 ? segments[segments.length - 1] : cleaned;
-		})();
-
-		if (targetLineSlice.includes("=") && equalSperatorLine === -1) {
-			equalSperatorLine = i;
-		}
-
-		const blockData = getBlockData(targetLineSlice.trim(), equalSperatorLine);
-
-		if (blockData) {
-			blockType = blockData.blockType;
-			typeName = blockData.typeName;
-			break;
+		if (targetLineSlice.includes("=") && equalSeparatorLine === -1) {
+			equalSeparatorLine = i;
 		}
 
 		if (i === startLine) {
 			const cleanLine = targetLineSlice.slice(0, selection.start.character);
 
-			const arrayMatches = [...cleanLine.matchAll(/(\w+)\s*:\s*\[/g)];
-			arrayMatches.reverse().forEach((arrayMatch) => {
-				const tupleIndex = findTupleIndex(lines, i, startLine);
-				path.unshift(`${arrayMatch[1]}[${tupleIndex}]`);
+			const arrayMatches = [...cleanLine.matchAll(/(\w+)\s*:\s*\[/g)].sort(
+				(a, b) => b.index - a.index,
+			);
+			arrayMatches.forEach((arrayMatch, i) => {
+				console.log(`arrayMatch ${i}: `, arrayMatch);
+				const tupleIndex = findTupleIndex(
+					lines,
+					startLine,
+					startLine,
+					i === 0
+						? selection.start.character
+						: arrayMatches.slice(0, i).reduce((pV, cV) => pV + cV.index, 0),
+					arrayMatch.index,
+				);
+				path.unshift({
+					content: `${arrayMatch[1]}[${tupleIndex}]`,
+					line: startLine,
+					idx: arrayMatch.index,
+				});
 			});
 
-			const propertyMatches = [...cleanLine.matchAll(/(\w+)\s*:\s*\{/g)];
+			const propertyMatches = [
+				// ...cleanLine.matchAll(/(\w+)\s*:\s*\{/gi),
+				...cleanLine.matchAll(/['"]?(\w+)['"]?\s*:\s*\{/gi),
+			];
 			propertyMatches.reverse().forEach((propertyMatch) => {
-				path.unshift(propertyMatch[1]);
+				path.unshift({
+					content: propertyMatch[1],
+					line: startLine,
+					idx: propertyMatch.index,
+				});
 			});
 		} else {
 			const lineLevel = getIndentationLevel(line);
 			if (lineLevel < lastLineLevel) {
-				const arrayMatches = [...targetLineSlice.matchAll(/(\w+)\s*:\s*\[/g)];
-				arrayMatches.reverse().forEach((arrayMatch) => {
-					const tupleIndex = findTupleIndex(lines, i, startLine);
-					path.unshift(`${arrayMatch[1]}[${tupleIndex}]`);
+				const arrayMatches = [
+					...targetLineSlice.matchAll(/(\w+)\s*:\s*\[/g),
+				].sort((a, b) => b.index - a.index);
+				arrayMatches.forEach((arrayMatch) => {
+					const tupleIndex = findTupleIndex(
+						lines,
+						i,
+						startLine,
+						i === 0
+							? selection.start.character
+							: arrayMatches.slice(0, i).reduce((pV, cV) => pV + cV.index, 0),
+						arrayMatch.index + arrayMatch[0].length,
+					);
+					path.unshift({
+						content: `${arrayMatch[1]}[${tupleIndex}]`,
+						line: i,
+						idx: arrayMatch.index,
+					});
 				});
 
 				const propertyMatches = [
-					...targetLineSlice.matchAll(/(\w+)\s*:\s*\{/gi),
+					// ...targetLineSlice.matchAll(/(\w+)\s*:\s*\{/gi),
+					...targetLineSlice.matchAll(/['"]?(\w+)['"]?\s*:\s*\{/gi),
 				];
 				propertyMatches.reverse().forEach((propertyMatch) => {
-					path.unshift(propertyMatch[1]);
+					path.unshift({
+						content: propertyMatch[1],
+						line: i,
+						idx: propertyMatch.index,
+					});
 				});
 
 				lastLineLevel = lineLevel;
 			}
+		}
+
+		const blockData = getBlockData(targetLineSlice.trim(), equalSeparatorLine);
+		if (blockData) {
+			blockType = blockData.blockType;
+			typeName = blockData.typeName;
+			break;
 		}
 	}
 
@@ -133,23 +253,7 @@ export function getKeyPathAtJSOrTS(
 		return { path: "", error: "Sorry, invalid key at inline type" };
 	}
 
-	const result =
-		blockType === "object" || blockType === "class"
-			? typeName + "." + path.join(".")
-			: typeName +
-			  "" +
-			  path
-					.map((p) =>
-						!isNaN(Number(p))
-							? `[${p}]`
-							: p.endsWith("]")
-							? p
-									.split("[")
-									.map((p2) => (p2.includes("]") ? `[${p2}` : `["${p2}"]`))
-									.join("")
-							: `["${p}"]`,
-					)
-					.join("");
+	const result = buildFinalPath(path, blockType, typeName);
 
 	return { path: result ?? "", error: "" };
 }
